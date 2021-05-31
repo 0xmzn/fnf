da8420cc2443464bafde34e6686d0b5486b0e59a
pseudomar <omaryasserhamed@gmail.com>
2021-05-31 19:59:15 +0200

    Fix bugs

---
+++ src/bird.hpp
@@ -0,0 +1,28 @@
+#ifndef bird_H
+#define bird_H
+#include <SFML/Graphics.hpp>
+using namespace sf;
+using namespace std;
+
+struct Flappy
+{
+	float x = 150, y = 200, velocity = 0;
+	bool isAlive = 1;
+	const float gravAcc = 0.010;
+	const float jumpAcc = -1;
+	Sprite sprite;
+	Texture texture[3];
+};
+
+Flappy flappy;
+
+void loadFlappy() {
+
+	flappy.texture[0].loadFromFile("assets/gnu.png");
+	flappy.texture[1].loadFromFile("assets/gnu.png");
+	flappy.texture[2].loadFromFile("assets/gnu.png");
+	flappy.sprite.setScale(0.3, 0.3);
+	flappy.sprite.setTexture(flappy.texture[1]);
+	flappy.sprite.setPosition(flappy.x, flappy.y);
+}
+#endif // bird_H
---
+++ src/main.cpp
@@ -0,0 +1,153 @@
+#include "bird.hpp"
+#include "displayingEvents.hpp"
+#include "pipe.hpp"
+#include <SFML/Graphics.hpp>
+#include <bits/stdc++.h>
+using namespace sf;
+using namespace std;
+
+int main()
+{
+	// Create a window
+	RenderWindow window(VideoMode(WIDTH, HEIGHT), "fnf", Style::Default);
+	window.setFramerateLimit(360);
+	Texture backgroundImage;
+	backgroundImage.loadFromFile("assets/background.png");
+	Sprite background(backgroundImage);
+
+	// Load flappy and pipe
+	loadFlappy();
+	loadPipe();
+
+	int frameRate = 150, gap = 225; // default
+
+	// Game Loop
+	while (window.isOpen())
+	{
+		Event event;
+		handleEvent(window, event);
+
+		// Startscreen or if esc is pressed
+
+		if (gameLevel == 0)
+		{
+			displayMenu(window);
+		}
+		else
+		{
+			// easy
+			if (gameLevel == 1)
+			{
+				frameRate = 200;
+				gap = 250;
+			}
+			// medium
+			if (gameLevel == 500)
+			{
+				frameRate = 150;
+				gap = 225;
+			}
+			// hard
+			if (gameLevel == 999)
+			{
+				frameRate = 120;
+				gap = 200;
+			}
+
+			// generate pipes
+			if (frames % frameRate == 0)
+			{
+				// dark magic
+				int random = rand() % 150 + 200;
+
+				// set position of new pipes
+				positionPipe(random, gap);
+
+				// push pipes to the vector
+				pipes.push_back(pipeDown.sprite);
+				pipes.push_back(pipeUp.sprite);
+			}
+            
+			// simulate moving
+			for (auto itr = pipes.begin(); itr != pipes.end(); itr++)
+			{
+				(*itr).move(-3, 0);
+
+			}
+
+			// remove pipes offscreen
+			if (frames % 100 == 0)
+			{
+				auto startIterator = pipes.begin();
+				auto endIterator = pipes.begin();
+
+				for (; endIterator != pipes.end(); endIterator++)
+				{
+					if ((*endIterator).getPosition().x > -104)
+					{
+						break;
+					}
+				}
+
+				pipes.erase(startIterator, endIterator);
+			}
+
+			for (auto i : pipes)
+			{
+				float px, py, pw, ph;
+
+				px = i.getPosition().x;
+				pw = 150 * i.getScale().x; // pipe width
+
+				if (i.getScale().y > 0)
+				{
+					py = i.getPosition().y;
+					ph = 800 * i.getScale().y; // pipe height
+				}
+				else
+				{
+					ph = -800 * i.getScale().y; // pipe height
+					py = i.getPosition().y - ph;
+				}
+
+				if (isColliding(flappy.sprite.getPosition().x, flappy.sprite.getPosition().y, flappy.sprite.getScale().x, flappy.sprite.getScale().y, px, py, pw, ph))
+				{
+					flappy.isAlive = false;
+				}
+			}
+
+			if (flappy.sprite.getPosition().y >= 700 || !flappy.isAlive)
+			{
+				flappy.isAlive = 0;
+
+				//	add gameOver screen
+				break;
+			}
+
+			// gravity effect
+			if (flappy.isAlive)
+			{
+				flappy.velocity += flappy.gravAcc;
+				flappy.sprite.move(0, flappy.velocity);
+			}
+
+			// draw bird and pipes
+
+			window.clear();
+			window.draw(background);
+			window.draw(flappy.sprite);
+
+			for (auto i : pipes)
+			{
+				window.draw(i);
+			}
+
+			window.display();
+
+			// increment frames
+			frames++;
+		}
+	}
+
+	return 0;
+}
